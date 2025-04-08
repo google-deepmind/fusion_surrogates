86caebe7327db2fa86867149f3d92e8e7a3ae495
Philippe Hamel <hamelphi@google.com>
2025-04-08 09:15:52 -0700

    Add model registry
    This will allow models to be loaded by name rather than having to specify a path.
    
    PiperOrigin-RevId: 745172407

--- fusion_surrogates/models/qlknn_7_11_test.py
+++ fusion_surrogates/models/qlknn_7_11_test.py
@@ -19,6 +19,7 @@ import os
 from absl.testing import absltest
 from absl.testing import parameterized
 from fusion_surrogates import qlknn_model
+from fusion_surrogates.models import registry
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -28,7 +29,6 @@ from numpy import testing
 jax.config.update("jax_enable_x64", True)
 
 
-MODEL_FILENAME = "qlknn_7_11.qlknn"
 TEST_DATA_FILENAME = "qlknn_7_11_test_data.npz"
 
 
@@ -45,40 +45,45 @@ def _get_test_inputs_and_targets():
 
 class Qlknn711Test(parameterized.TestCase):
 
-  def setUp(self):
-    super().setUp()
-    model_path = os.path.join(
-        os.path.dirname(__file__),
-        MODEL_FILENAME,
-    )
-    self._model = qlknn_model.QLKNNModel.import_model(model_path)
-
   def test_nan(self):
-    self.assertFalse(self._model.has_nan())
+    model = qlknn_model.QLKNNModel.load_default_model()
+    self.assertFalse(model.has_nan())
 
   @parameterized.parameters(1, 10, 100)
   def test_shape(self, batch_size):
-    inputs = np.empty((1, batch_size, self._model.num_inputs))
-    outputs = self._model.predict_targets(inputs)
-    self.assertEqual(outputs.shape, (1, batch_size, self._model.num_targets))
+    model = qlknn_model.QLKNNModel.load_default_model()
+    inputs = np.empty((1, batch_size, model.num_inputs))
+    outputs = model.predict_targets(inputs)
+    self.assertEqual(outputs.shape, (1, batch_size, model.num_targets))
 
   @parameterized.parameters(*_get_test_inputs_and_targets())
   def test_numerical_numpy(self, inputs, targets):
+    model = qlknn_model.QLKNNModel.load_default_model()
     self.assertTrue(jax.config.jax_enable_x64)
     self.assertEqual(inputs.dtype, np.float64)
     self.assertEqual(targets.dtype, np.float64)
-    preds = self._model.predict_targets(inputs)
+    preds = model.predict_targets(inputs)
     self.assertEqual(preds.dtype, np.float64)
     testing.assert_array_almost_equal(preds, targets, decimal=10)
 
   @parameterized.parameters(*_get_test_inputs_and_targets())
   def test_numerical_jitted(self, inputs, targets):
+    model = qlknn_model.QLKNNModel.load_default_model()
     self.assertTrue(jax.config.jax_enable_x64)
     inputs = jnp.array(inputs, dtype=jnp.float64)
     targets = jnp.array(targets, dtype=jnp.float64)
-    preds = jax.jit(self._model.predict_targets)(inputs)
+    preds = jax.jit(model.predict_targets)(inputs)
     self.assertEqual(preds.dtype, jnp.float64)
     testing.assert_array_almost_equal(preds, targets, decimal=10)
+
+  @parameterized.parameters(
+      registry.MODELS.keys()
+  )
+  def test_registry(self, model_name):
+    model = qlknn_model.QLKNNModel.load_model_from_name(model_name)
+    self.assertEqual(model.name, model_name)
+    self.assertEqual(model.path, registry.MODELS[model_name])
+    self.assertFalse(model.has_nan())
 
 
 if __name__ == "__main__":
